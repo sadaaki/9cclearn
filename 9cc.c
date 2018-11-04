88db8569a22f43dcf5f186f3783f5d58b8567abe
Sadaaki Kato <sadaaki@gmail.com>
2018-11-04 11:55:05 +0900

    トクナイザを追加（ごつかった）

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,63 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// トークンの型を表す値
+enum {
+	TK_NUM = 256, // 整数トークン
+	TK_EOF, // 入力の終わりを示すトークン
+};
+
+// トークンの型
+typedef struct {
+	int ty; // トークンの型
+	int val; // tyがTK_NUMの場合、その数値
+	char *input; // トークン文字列（エラーメッセージ用）
+} Token;
+
+
+Token tokens[100]; // トークナイズした結果を収める配列（最大100個とする）
+
+// pがさしている文字列をトークンに分割してtokensに保存する
+void tokenize(char *p) {
+	int i = 0;
+	while (*p) {
+		// skip white space
+		if (isspace(*p)) {
+			p++;
+			continue;
+		}
+
+		if (*p == '+' || *p =='-') {
+			tokens[i].ty = *p;
+			tokens[i].input = p;
+			i++;
+			p++;
+			continue;
+		}
+
+		if (isdigit(*p)) {
+			tokens[i].ty = TK_NUM;
+			tokens[i].input = p;
+			tokens[i].val = strtol(p, &p, 10);
+			i++;
+			continue;
+		}
+		
+		fprintf(stderr, "トークナイズできません: %s\n", p);
+		exit(1);
+	}
+
+	tokens[i].ty = TK_EOF;
+	tokens[i].input = p;
+}
+
+// エラーを報告するための関数
+void error(int i) {
+  fprintf(stderr, "予期せぬトークンです: %s\n", tokens[i].input);
+  exit(1);
+}
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
@@ -7,31 +65,47 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	char *p = argv[1];
+  // トークナイズする
+  tokenize(argv[1]);
 
+  // アセンブリの前半部を出力
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf("	mov rax, %ld\n", strtol(p, &p, 10));
-	
-	while (*p) {
-		if ( *p == '+' ) {
-			p++;
-			printf("	add rax, %ld\n", strtol(p, &p, 10));
-			continue;
-		}
 
-		if ( *p == '-' ) {
-			p++;
-			printf("	sub rax, %ld\n", strtol(p, &p, 10));
-			continue;
-		}
-
-		fprintf(stderr, "予期せぬ文字です: '%c'\n", *p);
-		return 1;
+  // 式の最初は必ず数字であるはずなので、チェックして最初のmov命令を出力
+	if (tokens[0].ty != TK_NUM) {
+   		error(0);
 	}
+	printf("	mov rax, %d\n", tokens[0].val);
+
+  // '+ <数>'あるいは'- <数>というトークンの並びをアセンブリに変換していく
 	
-	printf("	ret\n");
+  int i = 1;
+  
+  while (tokens[i].ty != TK_EOF) {
+    if (tokens[i].ty == '+') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+        error(i);
+      printf("	add rax, %d\n", tokens[i].val);
+      i++;
+      continue;
+    }
+	  if (tokens[i].ty == '-') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+        error(i);
+      printf("	sub rax, %d\n", tokens[i].val);
+      i++;
+      continue;
+    }
+
+
+    error(i);
+  }
+    
+  printf("	ret\n");
 	return 0;
 
 }
